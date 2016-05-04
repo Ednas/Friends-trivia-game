375a3ad2fbefe1b2921b7994e5f801d4c64c4e64
ErnaNeal <Erna@MyCFLRealEstate.com>
2016-05-04 01:38:45 -0400

    Changed answer to an array within the array, added score function and a reset button

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,77 +1,65 @@
+//I think I need this function to start the game
+window.onload = function() {
+
+}
+//Questions Array
 var questions 
 {
 {
 	"q1" :"How many seasons are there of Friends?"
-	"answer1" : "10 seasons"
-	"answer2" : "17 seasons"
-	"answer3" : "4 seasons" 
-	"answer4" : "9 seasons"
+	"answer" : ['10', '17', '4', '9'];
 }
 {
 	"q2" :"What acress played Rachel?"
-	"answer1" : "Jennifer Love Hewitt"
-	"answer2" : "Julia Roberts"
-	"answer3" : "Jennifer Aniston" 
-	"answer4" : "George Tekei"
+	"answer" : ['Jennifer Love Hewitt', 'Julia Roberts', 'Jennifer Aniston','George Tekei'];
 }
 {
 	"q3" : "What was the name of the coffee shop they used to go to?"
-	"answer1" : "Central Coffee"
-	"answer2" : "Central Park"
-	"answer3" : "Central Perk"
-	"answer4" : "Central Tea"
+	"answer" : ['Central Coffee', 'Central Park', 'Central Perk', 'Central Tea'];
 }
 {
 	"q4" : "What is Pheobe's signature song?"
-	"answer1" : "Robot Chicken"
-	"answer2" : "Dancing Cats"
-	"answer3" : "Warm Kitty"
-	"answer4" : "Smelly Cat"
+	"answer" : ['Robot Chicken', 'Dancing Cats', 'Warm Kitty', 'Smelly Cat'];
 }
 {
 	"q5" : "Which two characters are siblings?"
-	"answer1" : "Rachel and Joey"
-	"answer2" : "Rachel and Chandler"
-	"answer3" : "Pheobe and Ross"
-	"answer4" : "Rachel and Ross"
+	"answer1" : ['Rachel and Joey', 'Rachel and Chandler', 'Pheobe and Ross', 'Rachel and Ross'];
 }
 {
 	"q6" : "What is Ross Geller's docorate in?"
-	"answer1" : "Proctology"
-	"answer2" : "Palentology"
-	"answer3" : "History"
-	"answer4" : "Philosophy"
+	"answer" : ['Proctology', 'Palentology', 'History', 'Philosophy'];
 }
 {
 	"q7" : "What is Joey's catch-phrase?"
-	"answer1" : "Have mercy!"
-	"answer2" : "Holy macrel"
-	"answer3" : "How you doin'"
-	"answer4" : "Psych"
+	"answer" : ['Have mercy!', 'Holy macrel', 'How you doin', 'Psych'];
 }
 {
 	"q8" : "What is Chandler Bing's middle name?"
-	"answer1" : "Muriel"
-	"answer2" : "Matthew"
-	"answer3" : "Stan"
-	"answer4" : "Stark"	
+	"answer" : ['Muriel', 'Matthew', 'Stan', 'Stark'];	
 }
 {
 	"q9" : "Who does Monica marry and have adopted twins with?"
-	"answer1" : "Ross"
-	"answer2" : "Gunner"
-	"answer3" : "Chandler"
-	"answer4" : "Joey"
+	"answer" : ['Ross', 'Gunner', 'Chandler', 'Joey'];
 }
 {
 	"q10" : "Who gives birth to triplets?"
-	"answer1" : "Joey"
-	"answer2" : "Monica"
-	"answer3" : "Rachel"
-	"answer4" : "Pheobe"
+	"answer" : ['Joey', 'Monica', 'Rachel', 'Pheobe'];
 }
 }
+//This keeps track of the scores
+var function scores() {
+	var correct = 0;
+	var incorrect = 0;
+	var unanswered = 0;
+}
+
+//Just to see if the questions load
 console.log (questions[0])
+//Timer for the game
 var function timer(argument) {
 	
+}
+//Restarts the game
+var reset = {
+	$('resetbrn').clearInterval(counter);
 }
