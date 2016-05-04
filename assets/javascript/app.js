e18711bd8d7e3838ea322683198aa29f6d6e6b69
ErnaNeal <Erna@MyCFLRealEstate.com>
2016-05-03 22:11:13 -0400

    added js array with questions

---
+++ assets/javascript/app.js
@@ -0,0 +1,77 @@
+var questions 
+{
+{
+	"q1" :"How many seasons are there of Friends?"
+	"answer1" : "10 seasons"
+	"answer2" : "17 seasons"
+	"answer3" : "4 seasons" 
+	"answer4" : "9 seasons"
+}
+{
+	"q2" :"What acress played Rachel?"
+	"answer1" : "Jennifer Love Hewitt"
+	"answer2" : "Julia Roberts"
+	"answer3" : "Jennifer Aniston" 
+	"answer4" : "George Tekei"
+}
+{
+	"q3" : "What was the name of the coffee shop they used to go to?"
+	"answer1" : "Central Coffee"
+	"answer2" : "Central Park"
+	"answer3" : "Central Perk"
+	"answer4" : "Central Tea"
+}
+{
+	"q4" : "What is Pheobe's signature song?"
+	"answer1" : "Robot Chicken"
+	"answer2" : "Dancing Cats"
+	"answer3" : "Warm Kitty"
+	"answer4" : "Smelly Cat"
+}
+{
+	"q5" : "Which two characters are siblings?"
+	"answer1" : "Rachel and Joey"
+	"answer2" : "Rachel and Chandler"
+	"answer3" : "Pheobe and Ross"
+	"answer4" : "Rachel and Ross"
+}
+{
+	"q6" : "What is Ross Geller's docorate in?"
+	"answer1" : "Proctology"
+	"answer2" : "Palentology"
+	"answer3" : "History"
+	"answer4" : "Philosophy"
+}
+{
+	"q7" : "What is Joey's catch-phrase?"
+	"answer1" : "Have mercy!"
+	"answer2" : "Holy macrel"
+	"answer3" : "How you doin'"
+	"answer4" : "Psych"
+}
+{
+	"q8" : "What is Chandler Bing's middle name?"
+	"answer1" : "Muriel"
+	"answer2" : "Matthew"
+	"answer3" : "Stan"
+	"answer4" : "Stark"	
+}
+{
+	"q9" : "Who does Monica marry and have adopted twins with?"
+	"answer1" : "Ross"
+	"answer2" : "Gunner"
+	"answer3" : "Chandler"
+	"answer4" : "Joey"
+}
+{
+	"q10" : "Who gives birth to triplets?"
+	"answer1" : "Joey"
+	"answer2" : "Monica"
+	"answer3" : "Rachel"
+	"answer4" : "Pheobe"
+}
+}
+console.log (questions[0])
+var function timer(argument) {
+	
+}
