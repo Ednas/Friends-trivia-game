b40dd22c315578c4b96a549473d5a24fb8263161
Ednas <edna@edna.tech>
2016-11-06 22:45:39 -0500

    Added User choice var and images to questions

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -2,6 +2,7 @@
 	let correct = 0;
 	let incorrect = 0;
 	let unanswered = 0;
+	let choice;
 $(document).ready(function(){
 	//Text that says "Click to start the game"
 
@@ -10,17 +11,20 @@ var questions = [
 {
 	q1 :"How many seasons are there of Friends?",
 	answer : ['10', '17', '4', '9'],
-	correct : "10"
+	correct : "10",
+	images : "assets/images/friends.jpg"
 },
 {
 	q2 :"What acress played Rachel?",
 	answer : ['Jennifer Love Hewitt', 'Julia Roberts', 'Jennifer Aniston','George Tekei'],
-	correct : "Jennifer Aniston"
+	correct : "Jennifer Aniston",
+	images : "assets/images/friends.jpg"	
 },
 {
 	q3 : "What was the name of the coffee shop they used to go to?",
 	answer : ['Central Coffee', 'Central Park', 'Central Perk', 'Central Tea'],
-	correct : "Central Perk"
+	correct : "Central Perk",
+	images : "assets/images/friends.jpg"	
 }
 ]
 
@@ -34,6 +38,41 @@ function newGame(){
 	
 	alert("You will have 30 seconds for each question. \n You can now begin the game");
 	$(".timer").append("<b>Timer: 00:00</b>");
+
+	for (var i = 0; i < questions.length; i++) {
+		$('#answer-button').click()
+	//If correct answer
+		//Get click event
+		//add 1 to correct counter
+		//show correct answer message
+		//Give 5 seconds before moving to next question
+		//If last question, go to final message
+	//If wrong answer
+		//Get click event
+		//add 1 to incorrect counter
+		//show incorrect answer message
+		//Give 5 seconds before moving to next question
+		//If last question, go to final message
+	//If unanswered
+		//Get click event
+		//add 1 to unanswered counter
+		//show unanswered answer message
+		//Give 5 seconds before moving to next question
+		//If last question go to final message		
+
+		if (questions[i].correct === choice){
+			correct++;
+			console.log("Correct: " + correct + " | Incorrect: " + incorrect + " | Unanswered: " + unanswered);
+		}
+		else if (choice === null){
+			unanswered++;
+			console.log("Correct: " + correct + " | Incorrect: " + incorrect + " | Unanswered: " + unanswered);
+		}
+		else {
+			incorrect++;
+			console.log("Correct: " + correct + " | Incorrect: " + incorrect + " | Unanswered: " + unanswered);			
+		}
+	}
 }
 $('.start').click(newGame);
 
@@ -89,24 +128,7 @@ $('.start').click(newGame);
  //    }
  //  }
 
-	//If correct answer
-		//Get click event
-		//add 1 to correct counter
-		//show correct answer message
-		//Give 5 seconds before moving to next question
-		//If last question, go to final message
-	//If wrong answer
-		//Get click event
-		//add 1 to incorrect counter
-		//show incorrect answer message
-		//Give 5 seconds before moving to next question
-		//If last question, go to final message
-	//If unanswered
-		//Get click event
-		//add 1 to unanswered counter
-		//show unanswered answer message
-		//Give 5 seconds before moving to next question
-		//If last question go to final message
+
 
 	//After last question, 
 		//show counter message
