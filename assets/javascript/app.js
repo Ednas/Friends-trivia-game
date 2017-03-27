4c1ae46c543ef408cff1063a479766fafaeae290
Ednas <edna@edna.tech>
2017-03-26 22:20:13 -0400

    Working on displaying questions for trivia game

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -3,40 +3,47 @@ let correct = 0;
 let incorrect = 0;
 let unanswered = 0;
 let choice;
+let countStartNumber = 0;
 
 //Text that says "Click to start the game"
 
 //Questions Array
 var questions = [{
-  q1: "How many seasons are there of Friends?",
+  q: "How many seasons are there of Friends?",
   answer: ['10', '17', '4', '9'],
   correct: "10",
   images: "assets/images/friends.jpg"
 }, {
-  q2: "What acress played Rachel?",
+  q: "What acress played Rachel?",
   answer: ['Jennifer Love Hewitt', 'Julia Roberts', 'Jennifer Aniston', 'George Tekei'],
   correct: "Jennifer Aniston",
   images: "assets/images/friends.jpg"
 }, {
-  q3: "What was the name of the coffee shop they used to go to?",
+  q: "What was the name of the coffee shop they used to go to?",
   answer: ['Central Coffee', 'Central Park', 'Central Perk', 'Central Tea'],
   correct: "Central Perk",
   images: "assets/images/friends.jpg"
-}]
-
-console.log(questions[0].q1);
-console.log(questions[1].answer);
-console.log(questions[2].correct);
-
-
+}];
 
 function newGame() {
 
   alert("You will have 30 seconds for each question. \n You can now begin the game");
   $(".timer").append("<b>Timer: 00:00</b>");
 
+
+    // $('#answer-button').click()
+     timer = setInterval(game.countdown, 1000);
+     let panel = document.getElementsByClassName('.question');
+     console.log(questions[0].q);
+     $(".question").html('<h2>' + questions[0].q + '</h2>' );
+     // for (var i = 0; i<questions[this.q].answers.length; i++){
+     //   panel.append('<button class="answer-button" id="button"' + 'data-name="' + questions[this.currentQuestion].answers[i] + '">' + questions[this.currentQuestion].answers[i]+ '</button>');
+     // }
+
   for (var i = 0; i < questions.length; i++) {
-    $('#answer-button').click()
+console.log(questions[i].q);
+console.log(questions[i].answer);
+console.log(questions[i].correct);
       //If correct answer
       //Get click event
       //add 1 to correct counter
@@ -68,6 +75,8 @@ function newGame() {
     }
   }
 }
+ //end newGame function
+
 $('.start').click(newGame);
 
 // 	//Button that starts the game
@@ -76,10 +85,11 @@ $(".question").each(function(index, questions) {
 
   for (var i = 0; i < questions.length; i++) {
     questions[i]
+    console.log(questions[i]);
   }
 
   $(".questions").show("complete");
-  console.log('WOw');
+  console.log('Wow');
 });
 
 
@@ -87,7 +97,7 @@ $(".question").each(function(index, questions) {
 
 var game = {
     questions: questions,
-    currentQuestion,
+    currentQuestion: questions[0],
     counter: countStartNumber,
     correct: 0,
     incorrect: 0,
@@ -99,16 +109,20 @@ var game = {
         console.log('TIME UP');
         game.timeUp();
       }
-    }
+    
   },
-  // //timer needs to start
-  // // timer counts down from 30sec
-  //  loadQuestion: function(){
-  //    timer = setInterval(game.countdown, 1000);
-  //    panel.html('<h2>' + questions[this.currentQuestion].question + '</h2>' );
-  //    for (var i = 0; i<questions[this.currentQuestion].answers.length; i++){
-  //      panel.append('<button class="answer-button" id="button"' + 'data-name="' + questions[this.currentQuestion].answers[i] + '">' + questions[this.currentQuestion].answers[i]+ '</button>');
-  //    },
+  //timer needs to start
+  // timer counts down from 30sec
+   loadQuestion: function(){
+     timer = setInterval(game.countdown, 1000);
+     var panel = document.getElementByClassName('Timer');
+     console.log(panel);
+     panel.html('<h2>' + questions[this.currentQuestion].question + '</h2>' );
+     for (var i = 0; i<questions[this.currentQuestion].answers.length; i++){
+       panel.append('<button class="answer-button" id="button"' + 'data-name="' + questions[this.currentQuestion].answers[i] + '">' + questions[this.currentQuestion].answers[i]+ '</button>');
+     }
+     }
+     }//,
   //  timeUp: function (){
   //    clearInterval(timer);
   //    $('#counter-number').html(game.counter);
@@ -129,21 +143,18 @@ var game = {
   //After last question, 
   //show counter message
   //reset button underneath
-  function end() {
-    $('.score').append("<div>Correct Answers: " + correct + "\n");
-    $('.score').append("<div>Incorrect Answers: " + incorrect + "\n");
-    $('.score').append("<div>Timed out: " + unanswered + "\n");
-  };
+  // function end() {
+  //   $('.score').append("<div>Correct Answers: " + correct + "\n");
+  //   $('.score').append("<div>Incorrect Answers: " + incorrect + "\n");
+  //   $('.score').append("<div>Timed out: " + unanswered + "\n");
+  // };
 
 function reset() {
+  //TODO - Make a clear all divs 
   let correct = 0;
   let incorrect = 0;
   let unanswered = 0;
   //	$(correct, incorrect, unanswered) = 0;
-  console.log(correct);
+  console.log("score: " + correct + incorrect + unanswered);
 };
 $('.resetbtn').click(reset);
-
-$(document).ready(function() {
-
-});
