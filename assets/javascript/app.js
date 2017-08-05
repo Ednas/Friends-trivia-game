ee612db65fdda78a3cc1d7ce84a5dc6e44a29cf6
Ednas <edna@edna.tech>
2017-08-04 22:32:18 -0400

    Timer issues have been fixed, working on select answer

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -4,6 +4,8 @@ let incorrect = 0;
 let unanswered = 0;
 let choice;
 let countStartNumber = 0;
+let timeLeft = 30;
+
 
 //Questions Array
 var questions = [{
@@ -23,52 +25,65 @@ var questions = [{
   images: "assets/images/friends.jpg"
 }];
 
-for (var i = 0; i < questions.length; i++) {
-  // questions[i]
-  //Scroll through the questions
-  //Take users answer
-  //Compare with correct answer
-  //Add to score tally
-  console.log(questions[i].q);
-};
-
 function timer(){
-  // $(".timer").html("<b>30 Seconds left</b>");
-  var second = 1000;
-  let timeLeft = 10;
-function less(){
-  if (timeLeft == 1) {
-    console.log("Time is up");
-  clearTimeout(tid);    
-  } else {
-  timeLeft--;
-  console.log(timeLeft);
-  // return timeLeft;
-  }
-}
-
- var tid = setInterval(less, 1000);
   $(".timer").html("<b>" + timeLeft + " Seconds left</b>");
-  // console.log(timeLeft)
-    // setTimeout(function(){ x.value= "1 seconds" }, 1000); 
+  function less(){
+    if (timeLeft == 0) {
+      $(".timer").html("<b>0 Seconds left</b>");
+      alert("Time is up");
+      unanswered++;
+      $(".score").html("<div>Correct: "+ correct + " | Incorrect: " + incorrect + " | Unanswered: " + unanswered +" </div>");
+      clearTimeout(tid);    
+    } else {
+      timeLeft--;
+      $(".timer").html("<b>" + timeLeft + " Seconds left</b>");
+      console.log(timeLeft);
+    }
+  }
+ let tid = setInterval(less, 1000);
 }
 
-function abortTimer() { 
-  clearTimeout();
+//For each Question, reset the timer
+function nextQuestion(){
+  timeLeft = 30;
+    timer();
+
+  for (var i = 0; i < questions.length; i++) {
+
+     $(".question").html('<h2>' + questions[i].q + '</h2>' );
+
+     $(".answer").html('<form>'
+     + '<p><input type="radio" id="myRadio" value="'+questions[i].answer[0]+'">  ' + questions[i].answer[0] + '</p>'
+     + '<p><input type="radio" id="myRadio" value="'+questions[i].answer[1]+'">  ' + questions[i].answer[1] + '</p>'
+     + '<p><input type="radio" id="myRadio" value="'+questions[i].answer[2]+'">  ' + questions[i].answer[2] + '</p>'
+     + '<p><input type="radio" id="myRadio" value="'+questions[i].answer[3]+'">  ' + questions[i].answer[3] + '</p>');
+//Figure a way to make the selected value equal the users choice
+      function chosen(){
+        alert('Clicked '+ questions[i].answer[i]);
+        choice = document.getElementById("myRadio").value;
+        console.log(choice);
+      }
+   }  
 }
+
 //Start function
 function startGame(){
   alert("You will have 30 seconds for each question. \n You can now begin the game");
-  timer();
+  nextQuestion();
 }
-$('.start').click(startGame);
-$('.clear').click(abortTimer);
 
-
-function resetScore(){
+function reset(){
   correct = 0;
   incorrect = 0;
   unanswered = 0;
   choice = null;
   countStartNumber = 0;
+  $(".timer").html("<div></div>");
+  $(".score").html("<div></div>");
+  $(".question").html("<div></div>");
+  $(".answer").html("<div></div>");
 }
+
+//Buttons
+$('.start').click(startGame);
+$('.resetbtn').click(reset);
