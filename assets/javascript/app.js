2fa0c2bc3403ec242ee686dd940f8d4da8fee267
Ednas <edna@edna.tech>
2018-04-07 21:59:46 -0400

    Changed from vanilla js to jQuery and added the function of selecting the answer

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,89 +1,134 @@
-//This keeps track of the scores
-let correct = 0;
-let incorrect = 0;
-let unanswered = 0;
-let choice;
-let countStartNumber = 0;
-let timeLeft = 30;
+var panel = $('#quiz-area');
+var countStartNumber = 30;
 
+///////////////////////////////////////////////////////////////////////////////
+//CLICK EVENTS
+///////////////////////////////////////////////////////////////////////////////
 
-//Questions Array
+$(document).on('click', '#start-over', function(e) {
+  game.reset();
+});
+
+$(document).on('click', '.answer-button', function(e) {
+  game.clicked(e);
+});
+
+$(document).on('click', '#start', function(e) {
+  $('#subwrapper').prepend('<h2>Time Remaining: <span id="counter-number">30</span> Seconds</h2>');
+  game.loadQuestion();
+});
+
+///////////////////////////////////////////////////////////////////////////////
+//Question set
+///////////////////////////////////////////////////////////////////////////////
 var questions = [{
-  q: "How many seasons are there of Friends?",
-  answer: ['10', '17', '4', '9'],
-  correct: "10",
-  images: "assets/images/friends.jpg"
+    question: "How many seasons are there of Friends?",
+    answers: ["Ten", "Seventeen", "Four", "Nine"],
+    correctAnswer: "Ten",
+    images: "assets/images/friends.jpg"
 }, {
-  q: "What acress played Rachel?",
-  answer: ['Jennifer Love Hewitt', 'Julia Roberts', 'Jennifer Aniston', 'George Tekei'],
-  correct: "Jennifer Aniston",
-  images: "assets/images/friends.jpg"
+    question: "What acress played Rachel?",
+    answers: ['Jennifer Love Hewitt', 'Julia Roberts', 'Jennifer Aniston', 'George Tekei'],
+    correctAnswer: "Jennifer Aniston",
+    images: "assets/images/friends.jpg"
 }, {
-  q: "What was the name of the coffee shop they used to go to?",
-  answer: ['Central Coffee', 'Central Park', 'Central Perk', 'Central Tea'],
-  correct: "Central Perk",
-  images: "assets/images/friends.jpg"
+    question: "What was the name of the coffee shop they used to go to?",
+    answers: ['Central Coffee', 'Central Park', 'Central Perk', 'Central Tea'],
+    correctAnswer: "Central Perk",
+    images: "assets/images/friends.jpg"
 }];
 
-function timer(){
-  $(".timer").html("<b>" + timeLeft + " Seconds left</b>");
-  function less(){
-    if (timeLeft == 0) {
-      $(".timer").html("<b>0 Seconds left</b>");
-      alert("Time is up");
-      unanswered++;
-      $(".score").html("<div><span class=scores well well-sm>Correct: <span class='badge'>"+ correct +"</span> | Incorrect: <span class='badge'>"+ correct +"</span> | Unanswered: <span class='badge'>" + unanswered + "</span> </span></div>");
-      clearTimeout(tid);    
-    } else {
-      timeLeft--;
-      $(".timer").html("<b>" + timeLeft + " Seconds left</b>");
-      console.log(timeLeft);
+var game = {
+  questions:questions,
+  currentQuestion:0,
+  counter:countStartNumber,
+  correct:0,
+  incorrect:0,
+  countdown: function(){
+    game.counter--;
+    $('#counter-number').html(game.counter);
+
+    if (game.counter === 0){
+      console.log('TIME UP');
+      game.timeUp();
     }
+  },
+  loadQuestion: function(){
+    timer = setInterval(game.countdown, 1000);
+    panel.html('<h2>' + questions[this.currentQuestion].question + '</h2>' );
+    for (var i = 0; i<questions[this.currentQuestion].answers.length; i++){
+      panel.append('<button class="answer-button" id="button"' + 'data-name="' + questions[this.currentQuestion].answers[i] + '">' + questions[this.currentQuestion].answers[i]+ '</button>');
+    }
+  },
+  nextQuestion: function(){
+    game.counter = countStartNumber;
+    $('#counter-number').html(game.counter);
+    game.currentQuestion++;
+    game.loadQuestion();
+  },
+  timeUp: function (){
+    clearInterval(timer);
+    $('#counter-number').html(game.counter);
+
+    panel.html('<h2>Out of Time!</h2>');
+    panel.append('<h3>The Correct Answer was: ' + questions[this.currentQuestion].correctAnswer);
+    panel.append('<img src="' + questions[this.currentQuestion].image + '" />');
+
+    if (game.currentQuestion === questions.length - 1){
+      setTimeout(game.results, 3 * 1000);
+    } else {
+      setTimeout(game.nextQuestion, 3 * 1000);
+    }
+  },
+  results: function() {
+    clearInterval(timer);
+
+    panel.html('<h2>All done, heres how you did!</h2>');
+    $('#counter-number').html(game.counter);
+    panel.append('<h3>Correct Answers: ' + game.correct + '</h3>');
+    panel.append('<h3>Incorrect Answers: ' + game.incorrect + '</h3>');
+    panel.append('<h3>Unanswered: ' + (questions.length - (game.incorrect + game.correct)) + '</h3>');
+    panel.append('<br><button id="start-over">Start Over?</button>');
+  },
+  clicked: function(e) {
+    clearInterval(timer);
+
+    if ($(e.target).data("name") === questions[this.currentQuestion].correctAnswer){
+      this.answeredCorrectly();
+    } else {
+      this.answeredIncorrectly();
+    }
+  },
+  answeredIncorrectly: function() {
+    game.incorrect++;
+    clearInterval(timer);
+    panel.html('<h2>Nope!</h2>');
+    panel.append('<h3>The Correct Answer was: ' + questions[game.currentQuestion].correctAnswer + '</h3>');
+    panel.append('<img src="' + questions[game.currentQuestion].image + '" />');
+
+    if (game.currentQuestion === questions.length - 1){
+      setTimeout(game.results, 3 * 1000);
+    } else {
+      setTimeout(game.nextQuestion, 3 * 1000);
+    }
+  },
+  answeredCorrectly: function(){
+    clearInterval(timer);
+    game.correct++;
+    panel.html('<h2>Correct!</h2>');
+    panel.append('<img src="' + questions[game.currentQuestion].image + '" />');
+
+    if (game.currentQuestion === questions.length - 1){
+      setTimeout(game.results, 3 * 1000);
+    } else {
+      setTimeout(game.nextQuestion, 3 * 1000);
+    }
+  },
+  reset: function(){
+    this.currentQuestion = 0;
+    this.counter = countStartNumber;
+    this.correct = 0;
+    this.incorrect = 0;
+    this.loadQuestion();
   }
- let tid = setInterval(less, 1000);
-}
-
-//For each Question, reset the timer
-function nextQuestion(){
-  timeLeft = 5;
-    timer();
-
-  for (var i = 0; i < questions.length; i++) {
-
-     $(".question").html('<h2>' + questions[i].q + '</h2>' );
-
-     $(".answer").html('<form>'
-     + '<p><input type="radio" id="myRadio" value="'+questions[i].answer[0]+'">  ' + questions[i].answer[0] + '</p>'
-     + '<p><input type="radio" id="myRadio" value="'+questions[i].answer[1]+'">  ' + questions[i].answer[1] + '</p>'
-     + '<p><input type="radio" id="myRadio" value="'+questions[i].answer[2]+'">  ' + questions[i].answer[2] + '</p>'
-     + '<p><input type="radio" id="myRadio" value="'+questions[i].answer[3]+'">  ' + questions[i].answer[3] + '</p>');
-//Figure a way to make the selected value equal the users choice
-      function chosen(){
-        alert('Clicked '+ questions[i].answer[i]);
-        choice = document.getElementById("myRadio").value;
-        console.log(choice);
-      }
-   }  
-}
-
-//Start function
-function startGame(){
-  alert("You will have 30 seconds for each question. \n You can now begin the game");
-  nextQuestion();
-}
-
-function reset(){
-  correct = 0;
-  incorrect = 0;
-  unanswered = 0;
-  choice = null;
-  countStartNumber = 0;
-  $(".timer").html("<div></div>");
-  $(".score").html("<div></div>");
-  $(".question").html("<div></div>");
-  $(".answer").html("<div></div>");
-}
-
-//Buttons
-$('.start').click(startGame);
-$('.resetbtn').click(reset);
+};
